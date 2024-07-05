779074d4232c05f6b8f74aa0fbcfa98a578e29ac
LikeDesign <pikalakolitapika@gmail.com>
2024-07-05 15:36:51 -0500

    Updates

---
+++ assets/BB-lE8eK.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DwKpSQth.js";import{g as r,o as t}from"./3Fk0mtgH.js";import"./D9zmt5cj.js";const p={__name:"404",setup(c){return(e,_)=>(t(),r(o,{codigo:"Error 404",description:"Not found"}))}};export{p as default};
